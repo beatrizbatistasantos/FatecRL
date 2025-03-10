b85463853365775b219537ac6b3faa4ccfd7ff5b
Beatriz Batista <byah24@outlook.com>
2025-03-10 10:09:15 -0300

    Programa Finalizado para entrega na lista

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.ForegroundColor = ConsoleColor.DarkGray;
 Console.WriteLine("Fatec Rubens Lara");
+Console.ResetColor();
